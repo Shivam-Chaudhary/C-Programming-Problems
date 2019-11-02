885a1fdceeb876d7082d47277846a887d42915d4
Shivam-Chaudhary <53958346+Shivam-Chaudhary@users.noreply.github.com>
2019-11-02 11:28:04 +0530

    Print Hello World Using Function

---
+++ HELLOFUN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()  // main function
+{ void print(void);  // function declaration or prototype
+   clrscr(); // to clear output screen
+   print(); // function calling
+   getch(); // to hold the output screen
+}
+ void print() // function defination
+{
+ printf("Hello World I am C");
+}
